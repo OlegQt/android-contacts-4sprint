64a42088498feb446eb4801202aa9dcd8995586c
OlegQt <xqjh@mail.ru>
2022-12-27 22:50:15 +0300

    Исправил замечания по классу BaseListDiffCallback

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -4,27 +4,22 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.DiffUtil;
 
-public class BaseListDiffCallback <T> extends DiffUtil.ItemCallback<T> implements ListDiffInterface<T> {
+public class BaseListDiffCallback <T extends ListDiffInterface<T>> extends DiffUtil.ItemCallback<T> {
 
     @Override
     public boolean areItemsTheSame(@NonNull T oldItem, @NonNull T newItem) {
-        return this.theSameAs(newItem);
+        return oldItem.theSameAs(newItem);
     }
 
     @Override
     public boolean areContentsTheSame(@NonNull T oldItem, @NonNull T newItem) {
-        return false;
+        return oldItem.equals(newItem);
     }
 
-
-    @Override
-    public boolean theSameAs(T element) {
-        return false;
-    }
 
     @Nullable
     @Override
     public Object getChangePayload(@NonNull T oldItem, @NonNull T newItem) {
-        return super.getChangePayload(oldItem, newItem);
+        return newItem;
     }
 }
