b38df7b1a58c55b786e233a7a41e1f36d0e5c1e2
OlegQt <xqjh@mail.ru>
2022-12-28 22:36:45 +0300

    Revert "Исправил замечания по классу BaseListDiffCallback"
    This reverts commit 64a42088498feb446eb4801202aa9dcd8995586c.

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -4,22 +4,27 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.DiffUtil;
 
-public class BaseListDiffCallback <T extends ListDiffInterface<T>> extends DiffUtil.ItemCallback<T> {
+public class BaseListDiffCallback <T> extends DiffUtil.ItemCallback<T> implements ListDiffInterface<T> {
 
     @Override
     public boolean areItemsTheSame(@NonNull T oldItem, @NonNull T newItem) {
-        return oldItem.theSameAs(newItem);
+        return this.theSameAs(newItem);
     }
 
     @Override
     public boolean areContentsTheSame(@NonNull T oldItem, @NonNull T newItem) {
-        return oldItem.equals(newItem);
+        return false;
     }
 
+
+    @Override
+    public boolean theSameAs(T element) {
+        return false;
+    }
 
     @Nullable
     @Override
     public Object getChangePayload(@NonNull T oldItem, @NonNull T newItem) {
-        return newItem;
+        return super.getChangePayload(oldItem, newItem);
     }
 }
